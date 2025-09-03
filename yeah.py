2434aabfd0efe3511f42be197dde65324a25dbee
Raynaldo Winson <winsonamadeus.kusuma@gmail.com>
2025-09-04 00:22:10 +0700

    implemented prototype of  install command

--- yeah.py
+++ yeah.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
 import argparse
+import os
+import subprocess
 import sys
 import requests
+
+from pathlib import Path
 
 parser = argparse.ArgumentParser(
     prog="yeah",
@@ -33,3 +37,35 @@ if args.command == "search":
             print('') # Newline
     else:
         print('Maybe try another keyword?')
+
+def check_package_exists(package_name: str):
+    rpc_url = f'https://aur.archlinux.org/rpc/v5/info/{package_name}'
+    r = requests.get(rpc_url)
+    data = r.json()
+
+    return data['resultcount'] > 0
+
+if args.command == "install":
+    if not args.keywords:
+        print("Aborting operation due to bad usage.")
+        print("Format: yeah install <keywords>")
+        sys.exit(1)
+
+    pkgname = args.keywords
+
+    if not check_package_exists(pkgname):
+        print("There is no package with that identifier!")
+        print("Try another keyword?")
+        sys.exit(1)
+
+    yeah_dir = Path('~/.yeah/').expanduser()
+    yeah_dir.mkdir(parents=True, exist_ok=True)
+    os.chdir(yeah_dir)
+
+    git_url = f'https://aur.archlinux.org/{pkgname}.git'
+    cmd = f'git clone {git_url}'
+    subprocess.run(cmd.split())
+
+    os.chdir(pkgname)
+    cmd = 'makepkg -si'
+    subprocess.run(cmd.split())
