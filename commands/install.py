d3a29906690090f94663ae2e387bf562961d6bbf
Raynaldo Winson <winsonamadeus.kusuma@gmail.com>
2025-09-04 11:03:25 +0700

    created a get_yeah_dir function in commands/install.py

--- commands/install.py
+++ commands/install.py
@@ -6,13 +6,16 @@ def check_package_exists(package_name: str):
 
     return data['resultcount'] > 0
 
+def get_yeah_dir():
+    return Path('~/.yeah/').expanduser()
+
 def run(pkgname: str):
     if not check_package_exists(pkgname):
         print("There is no package with that identifier!")
         print("Try another keyword?")
         sys.exit(1)
 
-    yeah_dir = Path('~/.yeah/').expanduser()
+    yeah_dir = get_yeah_dir()
     yeah_dir.mkdir(parents=True, exist_ok=True)
     os.chdir(yeah_dir)
 
